5f25daa589537832f71caa3178a406fe62e27faa
arachnothrone <js1000avf@gmail.com>
2019-05-08 23:44:02 -0400

    SDP client

--- rfcomm_client_sdp.py
+++ rfcomm_client_sdp.py
@@ -1,1 +1,25 @@
+"""
+SDP RFCOMM client
+"""
 
+import sys
+from bluetooth import *
+
+service_matches = find_service(name="SampleServer", uuid=SERIAL_PORT_CLASS)
+
+if len(service_matches) == 0:
+    print("Couldn't find the service")
+    sys.exit(0)
+
+first_match = service_matches[0]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
+print("Connecting to {}".format(host))
+
+sock = BluetoothSocket(RFCOMM)
+sock.connect((host, port))
+sock.send("PyBluez client: hi")
+data = sock.recv(80)
+print("Received: {}".format(data))
+sock.close()
